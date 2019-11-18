440a48439abe170280a77bb6c58575307d79d20e
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2019-11-19 00:29:56 +0100

    Update install-dependencies.sh

--- .ci/install-dependencies.sh
+++ .ci/install-dependencies.sh
@@ -1,5 +1,5 @@
 echo 'deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main' || sudo tee /etc/apt/sources.list.d/realsense-public.list
-add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo $(lsb_release -s) main"
+add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo $(lsb_release -cs) main"
 apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keys.gnupg.net:80 --recv-key C8B3A55A6F3EFCDE
 apt-get update -qq
 apt-get install librealsense2-dkms --allow-unauthenticated -y
