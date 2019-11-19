7739f9bb3daa9ed80750b1ea3508b3c9f5d547cd
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2019-11-19 21:55:09 +0100

    Update run-tests.sh

--- .ci/run-tests.sh
+++ .ci/run-tests.sh
@@ -3,4 +3,4 @@
   wget $bag_filename -P "records/"
   bag_filename="http://realsense-hw-public.s3-eu-west-1.amazonaws.com/rs-tests/D435i_Depth_and_IMU_Stands_still.bag"
   wget $bag_filename -P "records/"
-  python $CATKIN_WORKSPACE/src/realsense-ros/src/realsense-ros/realsense2_camera/scripts/rs2_test.py --all
+  python $CATKIN_WORKSPACE/src/realsense-ros/realsense2_camera/scripts/rs2_test.py --all
